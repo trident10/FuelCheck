9e270956ffb37cb4f7f068d7318644a249063669
Abhi <abhimanu.jindal@nab.com.au>
2021-03-09 23:44:52 +1100

    Add Intial project

---
+++ FuelCheck/FuelCheck/FuelCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelCheckApp.swift
+//  FuelCheck
+//
+//  Created by Abhimanu Jindal on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct FuelCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FuelCheck/FuelCheckTests/FuelCheckTests.swift
@@ -0,0 +1,33 @@
+//
+//  FuelCheckTests.swift
+//  FuelCheckTests
+//
+//  Created by Abhimanu Jindal on 9/3/21.
+//
+
+import XCTest
+@testable import FuelCheck
+
+class FuelCheckTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
